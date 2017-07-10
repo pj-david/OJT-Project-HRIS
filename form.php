9e5d5c6d377396828c32c09d7cc332c3f381cf09
Zebedee Jimenez <2150936@slu.edu.ph>
2017-07-10 10:31:08 +0800

    Employee form with adding capability.

--- form.php
+++ form.php
@@ -33,13 +33,39 @@ font-size: 16px;"> Last access : 30 May 2014 &nbsp; <a href="#" class="btn btn-d
            <!-- /. NAV TOP  -->
                 <nav class="navbar-default navbar-side" role="navigation">
             <div class="sidebar-collapse">
-                <ul class="nav" id="main-menu">					
+                <ul class="nav" id="main-menu">
                     <li>
                         <a  href="index.php"><i class="fa fa-dashboard fa-3x"></i> Dashboard</a>
                     </li>
-                    <li  >
-                        <a  href="table.php"><i class="fa fa-table fa-3x"></i> Table</a>
-                    </li>
+                    
+                    <li>
+                        <a href="#"><i class="fa fa-table fa-3x"></i> Table<span class="fa arrow"></span></a>
+                        <ul class="nav nav-second-level">
+                            <li>
+                                <a href="table.php">General Employee Data</a>
+                            </li>
+                            <li>
+                                <a href="fullTable.php">Full Employee Data</a>
+                            </li>
+                            <li>
+                                <a href="#">Second Level Link<span class="fa arrow"></span></a>
+                                <ul class="nav nav-third-level">
+                                    <li>
+                                        <a href="#">Third Level Link</a>
+                                    </li>
+                                    <li>
+                                        <a href="#">Third Level Link</a>
+                                    </li>
+                                    <li>
+                                        <a href="#">Third Level Link</a>
+                                    </li>
+
+                                </ul>
+                               
+                            </li>
+                        </ul>
+                      </li> 
+                    
                     <li  >
                         <a class="active-menu" href="form.php"><i class="fa fa-edit fa-3x"></i> Forms </a>
                     </li>					
@@ -54,7 +80,7 @@ font-size: 16px;"> Last access : 30 May 2014 &nbsp; <a href="#" class="btn btn-d
                 <div class="row">
                     <div class="col-md-12">
                      <h2>Forms Page</h2>   
-                        <h5>Welcome Jhon Deo , Love to see you back. </h5>
+                        <h5>Welcome user, This is where you add data into the table</h5>
                        
                     </div>
                 </div>
@@ -65,243 +91,53 @@ font-size: 16px;"> Last access : 30 May 2014 &nbsp; <a href="#" class="btn btn-d
                     <!-- Form Elements -->
                     <div class="panel panel-default">
                         <div class="panel-heading">
-                            Form Element Examples
+                            Employee Data input
                         </div>
                         <div class="panel-body">
                             <div class="row">
                                 <div class="col-md-6">
-                                    <h3>Basic Form Examples</h3>
+                                    <h3>Employee information</h3>
                                     <form role="form">
                                         <div class="form-group">
-                                            <label>Text Input</label>
-                                            <input class="form-control" />
-                                            <p class="help-block">Help text here.</p>
+                                            <label>Employee ID</label>
+                                            <input class="form-control" placeholder="" name="employeeId"/>
                                         </div>
                                         <div class="form-group">
-                                            <label>Text Input with Placeholder</label>
-                                            <input class="form-control" placeholder="PLease Enter Keyword" />
+                                            <label>First Name</label>
+                                            <input class="form-control" placeholder="e.g Juan" name="firstName"/>
                                         </div>
                                         <div class="form-group">
-                                            <label>Just A Label Control</label>
-                                            <p class="form-control-static">info@yourdomain.com</p>
+                                            <label>Middle Name</label>
+                                            <input class="form-control" placeholder="e.g Jimenez" name="middleName"/>
                                         </div>
                                         <div class="form-group">
-                                            <label>File input</label>
-                                            <input type="file" />
+                                            <label>Last Name</label>
+                                            <input class="form-control" placeholder="e.g Santos" name="lastName"/>
                                         </div>
                                         <div class="form-group">
-                                            <label>Text area</label>
-                                            <textarea class="form-control" rows="3"></textarea>
+                                            <label>Age</label>
+                                            <input class="form-control" placeholder="" name="age"/>
                                         </div>
                                         <div class="form-group">
-                                            <label>Checkboxes</label>
-                                            <div class="checkbox">
-                                                <label>
-                                                    <input type="checkbox" value="" />Checkbox Example One
-                                                </label>
-                                            </div>
-                                            <div class="checkbox">
-                                                <label>
-                                                    <input type="checkbox" value=""/>Checkbox Example Two
-                                                </label>
-                                            </div>
-                                            <div class="checkbox">
-                                                <label>
-                                                    <input type="checkbox" value=""/>Checkbox Example Three
-                                                </label>
-                                            </div>
-                                        </div>
-                                        <div class="form-group">
-                                            <label>Inline Checkboxes Examples</label>
-                                            <label class="checkbox-inline">
-                                                <input type="checkbox"/> One
-                                            </label>
-                                            <label class="checkbox-inline">
-                                                <input type="checkbox"/> Two
-                                            </label>
-                                            <label class="checkbox-inline">
-                                                <input type="checkbox"/> Three
-                                            </label>
-                                        </div>
-                                        <div class="form-group">
-                                            <label>Radio Button Examples</label>
-                                            <div class="radio">
-                                                <label>
-                                                    <input type="radio" name="optionsRadios" id="optionsRadios1" value="option1" checked />Radio Example One
-                                                </label>
-                                            </div>
-                                            <div class="radio">
-                                                <label>
-                                                    <input type="radio" name="optionsRadios" id="optionsRadios2" value="option2"/>Radio Example Two
-                                                </label>
-                                            </div>
-                                            <div class="radio">
-                                                <label>
-                                                    <input type="radio" name="optionsRadios" id="optionsRadios3" value="option3"/>Radio Example Three
-                                                </label>
-                                            </div>
-                                        </div>
-                                        
-                                        <div class="form-group">
-                                            <label>Select Example</label>
+                                            <label>Gender</label>
                                             <select class="form-control">
-                                                <option>One Vale</option>
-                                                <option>Two Vale</option>
-                                                <option>Three Vale</option>
-                                                <option>Four Vale</option>
+                                                <option name="gender">M</option>
+                                                <option name="gender">F</option>
                                             </select>
                                         </div>
                                         <div class="form-group">
-                                            <label>Multiple Select Example</label>
-                                            <select multiple class="form-control">
-                                                <option>One Vale</option>
-                                                <option>Two Vale</option>
-                                                <option>Three Vale</option>
-                                                <option>Four Vale</option>
-                                            </select>
+                                            <label>Address</label>
+                                            <input class="form-control" placeholder="Street, City, Province, Postal code" name="Address"/>
                                         </div>
-                                        <button type="submit" class="btn btn-default">Submit Button</button>
-                                        <button type="reset" class="btn btn-primary">Reset Button</button>
-
                                     </form>
-                                    <br />
-                                      <h3>With radio & checkboxes</h3>
-                                    <form>
-
-    <div class="form-group input-group">
-      <span class="input-group-addon">
-        <input type="checkbox" />
-      </span>
-      <input type="text" class="form-control" />
-    </div>
-                                         <div class="form-group input-group">
-      <span class="input-group-addon">
-        <input type="radio" />
-      </span>
-      <input type="text" class="form-control" />
-    </div>
-                                    </form>
-
-                                 
-    </div>
+                                </div>
                                 
                                 <div class="col-md-6">
-                                    <h3>Disabled Form State Examples</h3>
-                                    <form role="form">
-                                        <fieldset disabled="disabled">
-                                            <div class="form-group">
-                                                <label for="disabledSelect">Disabled input</label>
-                                                <input class="form-control" id="disabledInput" type="text" placeholder="Disabled input" disabled />
-                                            </div>
-                                            <div class="form-group">
-                                                <label for="disabledSelect">Disabled select </label>
-                                                <select id="disabledSelect" class="form-control">
-                                                    <option>Disabled select</option>
-                                                </select>
-                                            </div>
-                                            <div class="checkbox">
-                                                <label>
-                                                    <input type="checkbox" />Disabled Checkbox
-                                                </label>
-                                            </div>
-                                            <button type="submit" class="btn btn-primary">Disabled Button</button>
-                                        </fieldset>
-                                    </form>
-                                    <h3>Validation State Examples</h3>
-                                    <form role="form">
-                                        <div class="form-group has-success">
-                                            <label class="control-label" for="inputSuccess">Input with success</label>
-                                            <input type="text" class="form-control" id="inputSuccess">
-                                        </div>
-                                        <div class="form-group has-warning">
-                                            <label class="control-label" for="inputWarning">Input with warning</label>
-                                            <input type="text" class="form-control" id="inputWarning">
-                                        </div>
-                                        <div class="form-group has-error">
-                                            <label class="control-label" for="inputError">Input with error</label>
-                                            <input type="text" class="form-control" id="inputError">
-                                        </div>
-                                    </form>
-                                    <h3>Input Group Examples</h3>
-                                    <form role="form">
-                                        <div class="form-group input-group">
-                                            <span class="input-group-addon">@</span>
-                                            <input type="text" class="form-control" placeholder="Username">
-                                        </div>
-                                        <div class="form-group input-group">
-                                            <input type="text" class="form-control">
-                                            <span class="input-group-addon">.00</span>
-                                        </div>
-                                        <div class="form-group input-group">
-                                            <span class="input-group-addon"><i class="fa fa-eur"></i>
-                                            </span>
-                                            <input type="text" class="form-control" placeholder="Font Awesome Icon">
-                                        </div>
-                                        <div class="form-group input-group">
-                                            <span class="input-group-addon">$</span>
-                                            <input type="text" class="form-control">
-                                            <span class="input-group-addon">.00</span>
-                                        </div>
-                                        <div class="form-group input-group">
-                                            <input type="text" class="form-control">
-                                            <span class="input-group-btn">
-                                                <button class="btn btn-default" type="button"><i class="fa fa-search"></i>
-                                                </button>
-                                            </span>
-                                        </div>
-                                    </form>
-                                    <h3>Different Size Input Groups</h3>
-                                     <form role="form">
-  <div class=" form-group input-group input-group-lg">
-  <span class="input-group-addon">@</span>
-  <input type="text" class="form-control" placeholder="Username" />
-</div>
-
-<div class="form-group input-group">
-  <span class="input-group-addon">@</span>
-  <input type="text" class="form-control" placeholder="Username" />
-</div>
-
-<div class="form-group input-group input-group-sm">
-  <span class="input-group-addon">@</span>
-  <input type="text" class="form-control" placeholder="Username" />
-</div>
-                                         
-                                     </form>
-                                    <h3>Different Size Input Groups</h3>
-                                     <form role="form">
-                                  <div class="input-group">
-      <span class="form-group input-group-btn">
-        <button class="btn btn-default" type="button">Go!</button>
-      </span>
-      <input type="text" class="form-control" />
-    </div>
-<br />
-                                           <div class="input-group">
-     
-      <input type="text" class="form-control" />
-                                                <span class="form-group input-group-btn">
-        <button class="btn btn-default" type="button">Go!</button>
-      </span>
-    </div>
-                                         </form>
+                                    
                                 </div>
+                     <!-- End Form Elements -->
                             </div>
                         </div>
-                    </div>
-                     <!-- End Form Elements -->
-                </div>
-            </div>
-                <!-- /. ROW  -->
-                <div class="row">
-                    <div class="col-md-12">
-                        <h3>More Customization</h3>
-                         <p>
-                        For more customization for this template or its components please visit official bootstrap website i.e getbootstrap.com or <a href="http://getbootstrap.com/components/" target="_blank">click here</a> . We hope you will enjoy our template. This template is easy to use, light weight and made with love by binarycart.com 
-                        </p>
-                    </div>
-                </div>
-                <!-- /. ROW  -->
     </div>
              <!-- /. PAGE INNER  -->
             </div>
